b20f929928b881c52612cbb3a8352af7196c08aa
Manfred Niesert <nieseman@niesert.net>
2024-02-10 03:19:10 +0100

    Moving some files, DEB package
    Fixing directory structure.
    Provide script to build simplistic .deb package.

---
+++ src/traymenu/__init__.py
@@ -0,0 +1,3 @@
+from traymenu.config import Config, USAGE_STR, \
+         MenuItem, Separator, SubmenuStart, SubmenuEnd, MenuEntry
+from traymenu.exec import run_command, run_command_wrapper, debug_output
--- traymenu/__init__.py
+++
@@ -1,3 +0,0 @@
-from config import Config, USAGE_STR, \
-         MenuItem, Separator, SubmenuStart, SubmenuEnd, MenuEntry
-from exec import run_command, run_command_wrapper, debug_output
